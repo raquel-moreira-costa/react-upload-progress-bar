644c51ef69607bba38892472829dce5e19d21c7f
José Carlos <jjc.silva10@gmail.com>
2020-09-17 17:08:45 -0300

    Updates

---
+++ precache-manifest.17bbd64eecce3c48a6386f96239a47e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd0d0802adae38bae73feed9f55c40a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "398b61a53fc586fd3be5",
+    "url": "/static/css/main.e6109773.chunk.css"
+  },
+  {
+    "revision": "d4b7d444106aa36083e4",
+    "url": "/static/js/2.2f5c00f6.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.2f5c00f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "398b61a53fc586fd3be5",
+    "url": "/static/js/main.c1c0575d.chunk.js"
+  },
+  {
+    "revision": "34f32b431fdfd48939e3",
+    "url": "/static/js/runtime-main.70073c90.js"
+  }
+]);
